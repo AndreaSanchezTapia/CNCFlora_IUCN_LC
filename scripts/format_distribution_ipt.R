9424bef2a375c794fe402bbce7441d4e5bc598cd
Andrea Sánchez Tapia <katori@gmail.com>
2018-12-10 16:38:07 -0200

    formateando habitat también

--- scripts/format_distribution_ipt.R
+++ scripts/format_distribution_ipt.R
@@ -5,9 +5,9 @@ library(readr)
 #     group_by(id) %>%
 #     mutate(location = paste(locationID, collapse = "-")) %>%
 #     select(-locationID) %>% distinct() %>% ungroup()
-# library(tidyr)
-# library(stringr)
-# library(jsonlite)
+library(tidyr)
+library(stringr)
+library(jsonlite)
 # names(distribution)
 # ocurrence_remarks <- distribution %>% select(occurrenceRemarks) %>%
 #     #slice(10000:10100) %>%
@@ -35,5 +35,53 @@ library(readr)
 # omdf <- data.frame(omdf)
 # head(omdf)
 # names(distribution)
+# distribution_mod <- distribution %>% mutate(occurrenceRemarks = omdf$om_all)
+# write.csv(distribution_mod, "./ipt/distribution_modified.csv")
+
+
+###lifeform
+lf_habitat <-
+    read_delim("./ipt/speciesprofile.txt", delim = "\t", quote = "")
+names(lf_habitat)
+i <- 1
+lf <- list()
+hab <- list()
+veg <- list()
+for (i in seq_along(lf_habitat$lifeForm)) {
+    lf[[i]] <- data.frame(id = lf_habitat$id[i], lifeForm = NA)
+    hab[[i]] <- data.frame(id = lf_habitat$id[i], habitat = NA)
+    veg[[i]] <- data.frame(id = lf_habitat$id[i], vegetationType = NA)
+    if (!is.na(lf_habitat$lifeForm[i])) {
+        jason <- jsonlite::fromJSON(as.character(lf_habitat$lifeForm[i]))
+        if ("lifeForm" %in% names(jason)) lf[[i]] <- data.frame(id = lf_habitat$id[i], lifeForm = jason["lifeForm"])
+        if ("habitat" %in% names(jason)) hab[[i]] <- data.frame(id = lf_habitat$id[i], habitat = jason["habitat"])
+        if ("vegetationType" %in% names(jason)) veg[[i]] <- data.frame(id = lf_habitat$id[i], vegetationType = jason["vegetationType"])
+        }
+    }
+head(lf)
+head(veg)
+head(hab)
+lf2 <- lf %>%
+    purrr::map(~ mutate(., lifeForm = paste(lifeForm, collapse = "-"))) %>%
+    purrr::map(~distinct(.))
+lf3 <- bind_rows(lf2)
+veg2 <- veg %>%
+    purrr::map(~ mutate(., vegetationType = paste(vegetationType, collapse = "-"))) %>%
+    purrr::map(~distinct(.))
+veg3 <- bind_rows(veg2)
+
+hab2 <- hab %>%
+    purrr::map(~ mutate(., habitat = paste(habitat, collapse = "-"))) %>%
+    purrr::map(~distinct(.))
+hab3 <- bind_rows(hab2)
+
+lf_hab <- lf3 %>% left_join(veg3) %>% left_join(hab3)
+lf_habitat %>%
+    #filter(!is.na(lifeForm)) %>%
+    head(.)
+lf_hab %>%
+    #filter(lifeForm != "NA") %>%
+    head(.)
+write.csv(lf_hab, "./ipt/lf_hab_modified.csv")
 # distribution_mod <- distribution %>% mutate(occurrenceRemarks = omdf$om_all)
 # write.csv(distribution_mod, "./ipt/distribution_modified.csv")
