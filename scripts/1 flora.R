23a7a3e38fcb6464378c531c8ea0e7736cf4dcae
Andrea Sánchez Tapia <katori@gmail.com>
2018-12-10 16:39:20 -0200

    rewriting formatted flora

--- scripts/1 flora.R
+++ scripts/1 flora.R
@@ -1,16 +1,8 @@
-#Flora do Brasil first
+# Format Flora do Brasil first
 ##loads packages----
-library(readxl)
 library(dplyr)
 library(flora)
 library(readr)
-# read original data----
-treespp <- read_excel("./data/LeastConcern_BrazilEndemics_original.xlsx", sheet = 1)
-names(treespp)
-#especies <- purrr::map(especies, ~remove.authors(.)) %>% simplify2array()
-treespp <- treespp %>%
-    mutate(original_binomial = purrr::map(ScientificName, ~remove.authors(.)) %>%
-               simplify2array())
 
 #Downloads data from FdB----
 #library("downloader")
@@ -18,16 +10,22 @@ treespp <- treespp %>%
 #download(url = pag, destfile = "iptflora")
 #unzip("iptflora",exdir = "./ipt")
 
-
+#reads formatted distribution
+distribution <- read_csv("./ipt/distribution_modified.csv") %>% select(-1)
+head(distribution)
 
 #####------
-taxon <- read_delim("./ipt/taxon.txt", delim = "\t", quote = "")
-
+taxon <- read_tsv("./ipt/taxon.txt", quote = "", trim_ws = T)
+head(taxon)
 relationship <- read_delim("./ipt/resourcerelationship.txt",
-           delim = "\t", quote = "") %>% distinct()
+           delim = "\t", quote = "") %>%
+    distinct()
 
 ref <- read_delim("./ipt/reference.txt",delim = "\t", quote = "")
-lf_habitat <- read_delim("./ipt/speciesprofile.txt", delim = "\t", quote = "")
+#lf_habitat <- read_delim("./ipt/speciesprofile.txt", delim = "\t", quote = "")
+lf_mod <- read.csv("./ipt/lf_hab_modified.csv")
+
+length(unique(lf_habitat$id))
 types <- read_delim("./ipt/typesandspecimen.txt", delim = "\t", quote = "") %>%
     group_by(id) %>%
     mutate_all(.funs = function(x) paste(x, collapse = "-"))
@@ -41,23 +39,10 @@ vernacular <- read_delim("./ipt/vernacularname.txt", delim = "\t") %>%
 relacion <- unique(relationship$relationshipOfResource)
 relacion
 
-taxon_dist <- left_join(taxon, distribution)
-taxon_dist_ref <- left_join(taxon, distribution, ref)
-taxon_dist_ref_sp <- left_join(taxon_dist_ref, sp)
-taxon_dist_ref_sp_types <- left_join(taxon_dist_ref_sp, types_collapsed)
-
-test <- left_join(taxon_dist_ref_sp, types)
-all <- left_join(taxon_dist_ref_sp_types, vernacular_edit)
-all <- mutate(all, nombre = paste (genus, specificEpithet))
-
-#los nombres están aceitos en la flora?
-taxon.filt2 <- filter(all, nombre %in% especies)
-taxon.filt2 <- filter(all, nombre %in% especies, taxonRank == "ESPECIES")
-aceitos <- taxon.filt2 %>% filter(taxonomicStatus == "NOME_ACEITO")
-sinonimos <- taxon.filt2 %>%
-    filter(taxonomicStatus != "NOME_ACEITO") %>%
-    left_join(relationship) %>% group_by(id) %>%
-    mutate(sinonimo_de= relatedResourceID)
-sin.filt <- filter(all, id %in% sinonimos$sinonimo_de)
-sin.filt <- filter(all, id %in% sinonimos$sinonimo_de) %>% mutate(id_correct = id)
-
+taxon_dist               <- left_join(taxon, distribution)
+taxon_dist_ref           <- left_join(taxon_dist, ref)
+taxon_dist_ref_lfh       <- left_join(taxon_dist_ref, lf_habitat)
+taxon_dist_ref_lfh_types <- left_join(taxon_dist_ref_lfh, types)
+all                      <- left_join(taxon_dist_ref_lfh_types, vernacular)
+all <- mutate(all, nombre = paste(genus, specificEpithet)) %>% distinct()
+write.csv(all, "./ipt/all_flora")
