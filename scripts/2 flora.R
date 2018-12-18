52c1bb9242ba0c6ecc5737fa6df0fd96ad45afcc
Andrea Sánchez Tapia <katori@gmail.com>
2018-12-18 09:47:08 -0200

    separa flora 1 y 2, formateando resultados y después juntando con la flora. pequeños cambios en formato.

--- scripts/2 flora.R
+++ scripts/2 flora.R
@@ -10,13 +10,14 @@ treespp <- read_excel("./data/LeastConcern_BrazilEndemics_original.xlsx", sheet 
     rename(scientificName = ScientificName) %>%
     mutate(nombre = purrr::map(scientificName, ~remove.authors(.)) %>%
                simplify2array())
-names(treespp)
+treespp[642,] %>% View()
+any(is.na(treespp$scientificName))
 treesspp2 <- treespp %>%
     dplyr::select(Family, scientificName, nombre) %>%
     left_join(flora) %>%
     dplyr::select(
            Family,
-        scientificName,
+           scientificName,
            nombre,
            taxonID,
            acceptedNameUsageID,
@@ -32,7 +33,7 @@ treesspp2 <- treespp %>%
            #habitat,
            #locality
            )
-names(treesspp2)
+
 treesp3 <- treesspp2 %>%
     #new column final_name
     mutate(final_name = ifelse(taxonomicStatus == "NOME_ACEITO", scientificName, acceptedNameUsage)) %>%
@@ -44,10 +45,20 @@ treesp3 <- treesspp2 %>%
     #new column syn
     mutate(tax_notes = if_else(taxonomicStatus == "NOME_ACEITO", "name ok", "")) %>%
     mutate(tax_notes = if_else(taxonomicStatus == "SINONIMO", "synonym", tax_notes)) %>%
-    mutate(family = if_else(!is.na(family), Family, family)) %>%
+    mutate(family = if_else(!is.na(family), family, Family)) %>%
     #mutate(notes_fam = if_else(family != Family, "family changed", "")) %>%
-    dplyr::select(scientificName, taxonID, final_name, final_ID, notes,tax_notes, family)
-write.csv(treesp3, "./results/names_flora.csv")
+    dplyr::select(scientificName, taxonID, final_name, final_ID, notes,tax_notes, family) %>%
+    rename(original_name = scientificName,
+           original_ID = taxonID,
+           scientificName = final_name,
+           taxonID = final_ID)
+write.csv(treesp3, "./results/names_flora1.csv")
 
-library(dplyr)
-flora %>% filter(nombre == "Tibouchina pulchra") %>% View()
+names(treesp3)
+names(flora)
+treesp4 <- treesp3 %>%
+    left_join(flora) %>%
+dplyr::select(-c(8:31, 35:40, 44:46, 48:50))
+names(treesp4)
+write.csv(treesp4, "./results/names_flora.csv")
+treesp4 %>% View()
