0fca6debcf071e70327bd92bf805adf6cc8734ae
Andrea Sánchez Tapia <katori@gmail.com>
2018-12-19 12:24:52 -0200

    results con codigo del cncflora no del IUCN

--- scripts/7 vegetacao.R
+++ scripts/7 vegetacao.R
@@ -5,7 +5,7 @@ unique(finalaoo$vegetationType)
 veg_comp <- readxl::read_excel("./data/Tabela comparativa - Tipos de vegetação IUCN e Flora do Brasil-11-12-2018.xlsx") %>% data.frame()
 names(veg_comp) <- c("IUCN", "CNCFlora", "FB")
 head(veg_comp)
-veg_comp %>% count(FB, IUCN, CNCFlora)
+veg_comp %>% count(FB,CNCFlora) %>% View()
 unique(veg_comp$FB)
 
 w <- grepl(pattern = veg_comp$FB[1], x = finalaoo$vegetationType)
@@ -14,14 +14,17 @@ finalaoo$vegetationType[w]
 i <- 1
 aoo_veg <- finalaoo
 for (i in seq_along(unique(veg_comp$FB))) {
-    vegname <- unique(veg_comp$IUCN)[i]
-    gr <- grep(pattern = veg_comp$FB[i], x = finalaoo$vegetationType)
+    vegname <- unique(veg_comp$FB)[i]
+    data <- veg_comp %>% filter(FB == vegname)
+    print(data)
+    gr <- grep(pattern = vegname, x = finalaoo$vegetationType)
     aoo_veg[[vegname]]  <- ""
-    aoo_veg[[vegname]][gr]  <- vegname
-
+    aoo_veg[[vegname]][gr]  <- data$CNCFlora
 }
 names(aoo_veg)
+View(aoo_veg)
+
+aoo_veg2 <- aoo_veg %>% tidyr::unite(IUCN, 20:67, sep =" ")
 View(aoo_veg2)
 
-aoo_veg2 <- aoo_veg %>% tidyr::unite(IUCN, 20:68, sep ="")
 write.csv(aoo_veg2, "./results/aoo_veg.csv")
